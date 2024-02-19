df6650502ef40b41845e91da8f1b27f619353ce2
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-19 13:06:22 +0300

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import './Button.css';
+import { ButtonProps } from './Button.props';
+
+
+function Button({ children, ...props }: ButtonProps) {
+	return (
+		<button className='button accent' {...props}>{children}</button>
+	);
+}
+
+export default Button;
