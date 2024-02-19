f643e483fbd7f168bb838b00d89b4cc19d9d4c19
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-19 22:54:44 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css'
-import Button from './components/Button/Button'
+import { MouseEvent, useState } from 'react';
+import Button from './components/Button/Button/Button';
+import Input from './components/Button/Input/Input';
 
 function App() {
-  
+	const [counter, setCounter] = useState<number>();
 
-  return (
-    <>
-      <Button>Кнопка</Button>
-    </>
-  )
+	const addCounter = (e: MouseEvent) => {
+		console.log(e);
+
+	};
+
+	return (
+		<>
+			<Button onClick={addCounter}>Кнопка</Button>
+			<Button appearence='big'>Кнопка</Button>
+			<Input placeholder='Email'/>
+		</>
+	);
 }
 
-export default App
+export default App;
