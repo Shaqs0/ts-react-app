667d5a66f093992f6f3b93e3255fe70de7c1b61a
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-19 13:12:51 +0300

    .

--- src/components/Button/Button.props.ts
+++ src/components/Button/Button.props.ts
@@ -1,6 +1,6 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonProps extends ButtonHTMLAttributes{
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     children: ReactNode;
     
 }
