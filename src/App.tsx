fe0fe2a06a69d8f770c3ed5f60497a2e8a50d81d
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-20 19:44:18 +0300

    /

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button/Button';
 import Input from './components/Button/Input/Input';
+import { Link, RouterProvider} from 'react-router-dom';
 
 function App() {
-	const [counter, setCounter] = useState<number>();
+	const [counter, setCounter] = useState<number>(0);
 
 	const addCounter = (e: MouseEvent) => {
 		console.log(e);
-
 	};
 
 	return (
