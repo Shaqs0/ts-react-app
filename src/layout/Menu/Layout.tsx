d24dd5a543e248929fcba12ed3bef4cb7b4fa87c
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-25 16:50:52 +0300

    /

---
+++ src/layout/Menu/Layout.tsx
@@ -0,0 +1,32 @@
+import { Link, Outlet } from 'react-router-dom';
+import styles from './Layout.module.css';
+import Button from '../../components/Button/Button/Button';
+
+export function Layout() {
+	return <div className={styles['layout']}>
+		<div className={styles['sidebar']}>
+			<div className={styles['user']}>
+				<img className={styles['avatar']} src='/avatar.svg' alt="Аватар пользователя"/>
+				<div className={styles['name']}>Матвей Гончаров</div>
+				<div className={styles['email']}>goncharov@gmail.com</div>
+			</div>
+			<div className={styles['menu']}>
+				<Link to='/' className={styles['link']}>
+					<img src='/menu-icon.svg' alt="Иконка меню" />
+					Меню</Link>
+				<Link to='/cart' className={styles['link']}>
+					<img src='/cart-icon.svg' alt="Иконка корзины" />
+					Корзина</Link>
+			</div>
+			<Button className={styles['exit']}>
+				<img src="/exit-icon.svg" alt='Иконка выхода'/>
+				Выход
+			</Button>
+
+			
+		</div>
+		<div>
+			<Outlet />
+		</div>
+	</div>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,23 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Menu } from './pages/Menu/Menu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
+import { Layout } from './layout/Menu/Layout';
 
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Menu/>
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />
+			},
+			{
+				path: '/cart',
+				element: <Cart />
+			}
+		]
 	},
 	{
 		path: '/cart',
