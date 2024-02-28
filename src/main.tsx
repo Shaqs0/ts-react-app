c080cab1f034e7b6edb2f05e863c7a79fdee675b
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-28 23:53:31 +0300

    /

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
 import { Layout } from './layout/Menu/Layout';
 import { Product } from './pages/Product/Product';
+import axios from 'axios';
+import { PREFIX } from './helpers/API';
 
 
 const router = createBrowserRouter([
@@ -28,7 +30,11 @@ const router = createBrowserRouter([
 			},
 			{
 				path: '/product/:id',
-				element: <Product/>
+				element: <Product/>,
+				loader: async ({ params }) => {
+					const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
+					return data;
+				}
 			}
 			
 		]
