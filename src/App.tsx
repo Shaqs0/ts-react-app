bfc06ba7beef5d38b46c4476ed707d03835823d6
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-29 13:03:24 +0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+
+function App() {
+	return (
+		<>
+			<Button appearence='big'>Кнопка</Button>
+			<Input placeholder='Email'/>
+		</>
+	);
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,58 @@
+import React, { Suspense, lazy } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+import { Layout } from './layout/Menu/Layout';
+import { Product } from './pages/Product/Product';
+import axios from 'axios';
+import { PREFIX } from './helpers/API';
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
+
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Suspense fallback={<>Загрузка...</>}> <Menu /> </Suspense>
+			},
+			{
+				path: '/cart',
+				element: <Cart />
+			},
+			{
+				path: '/cart',
+				element: <Cart/>
+			},
+			{
+				path: '/product/:id',
+				element: <Product/>,
+				errorElement: <>Ошибка</>,
+				loader: async ({ params }) => {
+					const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
+					return data;
+				}
+			}
+			
+		]
+	},
+	{
+		path: '/cart',
+		element: <Cart />
+	},
+	{
+		path: '*',
+		element: <Error />
+	}
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<RouterProvider router = {router} />
+	</React.StrictMode>,
+);
