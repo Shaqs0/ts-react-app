9113c507310aaf7c661197290131ef6789b57933
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-29 13:06:42 +0300

    update

---
+++ src/components/Headling/Headling.props.ts
@@ -0,0 +1,5 @@
+import { InputHTMLAttributes} from 'react';
+
+export interface HeadlingProps extends InputHTMLAttributes<HTMLHeadingElement> {
+    
+}
---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,9 @@
+import { useLoaderData } from 'react-router-dom';
+import { Product } from '../../interfaces/product.interface';
+
+export function Product() {
+	const data = useLoaderData() as Product;
+
+
+	return <>Product - {data.name}</>;
+}
