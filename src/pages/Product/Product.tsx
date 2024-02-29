906d0aae4815102ede415358440a50b1ab10be52
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-29 13:03:08 +0300

    update

--- src/pages/Product/Product.tsx
+++
@@ -1,9 +0,0 @@
-import { useLoaderData } from 'react-router-dom';
-import { Product } from '../../interfaces/product.interface';
-
-export function Product() {
-	const data = useLoaderData() as Product;
-
-
-	return <>Product - {data.name}</>;
-}
