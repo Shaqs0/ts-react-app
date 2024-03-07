e2addb61a3f7c03aa51e3f16908a85611960a390
Shaqs0 <hhgfhhhggb@gmail.com>
2024-03-07 12:00:49 +0300

    /

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Button from './components/Button/Button';
-import Input from './components/Input/Input';
+
 
 function App() {
 	return (
 		<>
-			<Button appearence='big'>Кнопка</Button>
-			<Input placeholder='Email'/>
+			
 		</>
 	);
 }
